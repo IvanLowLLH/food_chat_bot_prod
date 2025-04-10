f76999034f423d6e4595248b78445f6632feb968
ivan <ivanlowlh@gmail.com>
2025-04-10 14:26:12 +0800

    Cleaned up get_location_queries.py and added documentation

--- app/get_location_queries.py
+++ app/get_location_queries.py
@@ -2,21 +2,29 @@ import difflib
 import json
 from typing import Dict
 
-from typing_extensions import List, Tuple
-
 class GetLocationSubzone:
     def __init__(self, area_file="area_to_subzone.json", subzone_file="sub_zone_nearby.json", match_cutoff=0.6):
+        """"
+        area_to_subzone is a dictionary where the key are Planning Areas, MRTs, shopping malls in Singapore and the value
+        is the relevant subzone
+        subzone_nearby is a dictionary where the keys are the sub-zones in Singapore and the values contain the
+        sub-zones surrounding it
+        """
         with open(area_file, "r", encoding="utf-8") as file:
             self.area_to_subzone = json.load(file)
         with open(subzone_file, "r", encoding="utf-8") as file:
             self.subzone_nearby = json.load(file)
         self.match_cutoff = match_cutoff
 
-    def _find_closest_match(self, word, word_list):
+    def _find_closest_match(self, word: str, word_list: list) -> str:
+        """Given a query word and a list of words, find a word in the list that matches the query word"""
         matches = difflib.get_close_matches(word, word_list, n=1, cutoff=self.match_cutoff)
         return matches[0] if matches else None
 
     def subzone_distance(self, base_zone, compare_zone):
+        """Given a base-zone and another zone (compare-zone), get the distance between them that
+        is already in subzone_nearby
+        """
         base_zone = base_zone.lower()
         compare_zone = compare_zone.lower()
         nearby_base_zone = self.subzone_nearby[base_zone]['nearest_subzone']
@@ -29,6 +37,7 @@ class GetLocationSubzone:
 
 
     def find_subzones(self, location_query: str, max_dist: float ) -> Dict:
+        """Given a location in Singapore, find the subzone it is in and nearby subzones"""
         location_query = location_query.lower()
         areas_places_list = self.area_to_subzone.keys()
         subzone_list = self.subzone_nearby.keys()
@@ -50,10 +59,6 @@ class GetLocationSubzone:
                 if distance <= max_dist:
                     result['nearby_subzones'].append(name.title())
         # Add original query at end. If no subzone match at all, then only return original query
-        # result.append((location_query, None, None))
         result['others'] = location_query
-        # Add matched name in case of misspell in location_query
-        # if area_place_match:
-        #     result.append((area_place_match.title(), None, None))
 
         return result
